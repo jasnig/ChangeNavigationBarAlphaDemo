f257469b01e6642f3513dbbac540dd2a17c3babe
ZeroJ <854136959@qq.com>
2016-05-29 18:03:34 +0800

    Initial Commit

---
+++ ChangeNaviBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChangeNaviBar
+//
+//  Created by jasnig on 16/5/29.
+//  Copyright © 2016年 ZeroJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
